83429d7b2bdf2b807d5e24de51b13cea747f4013
Abhishek khare <ghostabhi323@gmail.com>
2024-12-10 17:37:18 +0530

    refactor: Update recommended page UI and card layout

--- src/app/(pages)/recommended/page.jsx
+++ src/app/(pages)/recommended/page.jsx
@@ -8,7 +8,7 @@ import {
   CardTitle,
   CardFooter,
 } from "@/components/ui/card";
-import { Book, Play } from "lucide-react";
+import { Book, Play, ThumbsDown, ThumbsUp } from "lucide-react";
 
 const Page = () => {
   const [papers, setPapers] = useState([]);
@@ -45,49 +45,58 @@ const Page = () => {
       <div className="text-gray-500 text-sm pb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia modi soluta nam.</div>
       <div className="flex flex-col gap-3">
         {papers.map((paper) => (
-          <Card key={paper._id} className="relative shadow-sm hover:shadow transition-shadow duration-200 w-full flex rounded-sm bg-[#F9F9F9]">
-            <CardHeader className="relative flex-shrink-0">
-              <CardTitle>
-                <img
-                  src={paper.image_url || "/fallback-image.png"}
-                  alt={`Cover for ${paper.paper_title}`}
-                  className="w-full h-28 object-cover border"
-                  onError={(e) => {
-                    e.currentTarget.src = "/fallback-image.png";
-                  }}
-                />
-              </CardTitle>
-            </CardHeader>
-            <CardContent className="py-4 px-1 max-w-4xl">
-              <strong className="font-semibold text-gray-800 line-clamp-2">
-                {paper.paper_title}
-              </strong>
-              <div className="flex items-center gap-4 text-sm pt-1 pb-3">
-                <div><strong> Year : {" "}</strong>{paper.year || "N/A"}</div>
-                <div>
-                  <strong>Authors :</strong>{" "}
-                  {paper.author.length > 3
-                    ? `${paper.author
-                      .slice(0, 3)
-                      .map(
-                        (author) => `${author.first_name} ${author.last_name}`
-                      )
-                      .join(", ")}...`
-                    : paper.author
-                      .map(
-                        (author) => `${author.first_name} ${author.last_name}`
-                      )
-                      .join(", ")}
-                </div>
-                <div><strong> Paper Type : {" "}</strong>{paper.paper_type}</div>
-              </div>
+          <Card key={paper._id} className="relative shadow-sm hover:shadow transition-shadow duration-200 w-full flex rounded-sm bg-[#F9F9F9] items-center justify-between pr-3">
+            <div className="flex items-center">
 
-              <div className="line-clamp-2 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia ab nisi laboriosam expedita dicta unde porro modi necessitatibus quis. Repellendus atque perspiciatis rerum ea nam odit, fuga minus iste laborum temporibus omnis!</div>
-            </CardContent>
-            <CardFooter className="flex items-center">
-              <div className="text-xs text-gray-500"><Play /></div>
-              <Link href={`/recommended/${paper._id}`} className="text-xs bg-[#59C009] text-gray-50 px-3 py-2 flex items-center justify-center gap-1 rounded-full"><Book className="size-4" /> Read</Link>
-            </CardFooter>
+              <CardHeader className="relative flex-shrink-0">
+                <CardTitle>
+                  <img
+                    src={paper.image_url || "/fallback-image.png"}
+                    alt={`Cover for ${paper.paper_title}`}
+                    className="w-full h-28 object-cover border"
+                    onError={(e) => {
+                      e.currentTarget.src = "/fallback-image.png";
+                    }}
+                  />
+                </CardTitle>
+              </CardHeader>
+              <CardContent className="py-4 px-1 max-w-4xl">
+                <strong className="font-semibold text-gray-800 line-clamp-2">
+                  {paper.paper_title}
+                </strong>
+                <div className="flex items-center gap-4 text-sm pt-1 pb-3">
+                  <div><strong> Year : {" "}</strong>{paper.year || "N/A"}</div>
+                  <div>
+                    <strong>Authors :</strong>{" "}
+                    {paper.author.length > 3
+                      ? `${paper.author
+                        .slice(0, 3)
+                        .map(
+                          (author) => `${author.first_name} ${author.last_name}`
+                        )
+                        .join(", ")}...`
+                      : paper.author
+                        .map(
+                          (author) => `${author.first_name} ${author.last_name}`
+                        )
+                        .join(", ")}
+                  </div>
+                  <div><strong> Paper Type : {" "}</strong>{paper.paper_type}</div>
+                </div>
+
+                <div className="line-clamp-2 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia ab nisi laboriosam expedita dicta unde porro modi necessitatibus quis. Repellendus atque perspiciatis rerum ea nam odit, fuga minus iste laborum temporibus omnis!</div>
+              </CardContent>
+            </div>
+            <div className="flex flex-col items-end justify-end">
+              <div className="flex items-center gap-3 my-3 justify-end">
+                <div className="text-[#59C009] p-1.5 bg-gray-100 rounded-full"><ThumbsUp className="size-5" /></div>
+                <div className="text-gray-500 p-1.5 bg-gray-100 rounded-full"><ThumbsDown className="size-5" /></div>
+              </div>
+              <div className="flex items-center justify-end">
+                <div className="text-[#59C009] p-1.5 border-2 border-[#59C009] rounded-full mr-4"><Play className="size-5" /></div>
+                <Link href={`/recommended/${paper._id}`} className="text-base font-semibold bg-[#59C009] text-gray-50 px-5 py-2 flex items-center justify-center gap-1 rounded-full"><Book className="size-5" /> Read</Link>
+              </div>
+            </div>
           </Card>
         ))}
       </div>
