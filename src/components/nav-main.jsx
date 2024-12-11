722ae1c792107dca6432dd03722c13f8edc4bfdd
Abhishek khare <ghostabhi323@gmail.com>
2024-12-11 16:34:38 +0530

    refactor: Update sidebar links to match new page URLs

--- src/components/nav-main.jsx
+++ src/components/nav-main.jsx
@@ -16,28 +16,41 @@ import {
   SidebarMenuSub,
   SidebarMenuSubButton,
   SidebarMenuSubItem,
+  useSidebar,
 } from "@/components/ui/sidebar"
 import { Separator } from "./ui/separator";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+import { useState } from "react";
 
 export function NavMain({
   items
 }) {
   const pathname = usePathname();
+  const router = useRouter();
+  const { open } = useSidebar();
+
+  const [openIndex, setOpenIndex] = useState(null);
+
+  const handleToggle = (index) => {
+    setOpenIndex(openIndex === index ? null : index);
+  };
+
   return (
     (<SidebarGroup>
       <Separator className="mb-3 -mt-3" />
       <SidebarMenu>
-        {items.map((item) => (
+        {items.map((item, index) => (
           <Collapsible
             key={item.title}
             asChild
             defaultOpen={item.isActive}
+            open={openIndex === index}
+            onOpenChange={() => handleToggle(index)}
             className="group/collapsible">
             <SidebarMenuItem>
               <CollapsibleTrigger asChild>
-                <SidebarMenuButton tooltip={item.title}>
+                <SidebarMenuButton tooltip={item.title} onClick={() => !open && router.push(item.url)}>
                   {item.icon && <item.icon />}
                   <span>{item.title}</span>
                   <ChevronRight
