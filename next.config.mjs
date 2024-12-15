d19ea4ae07131fd938ff1e1ef19b12638a3c4469
Krishna <krishnacool781@gmail.com>
2024-12-15 17:00:02 +0530

    modified:   Dockerfile 	new file:   docker-compose.yaml 	modified:   next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,27 @@
 import 'dotenv/config';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-    NEXTAUTH_SECRET:process.env.NEXTAUTH_SECRET
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   },
   images: {
-    domains: ["minio.catax.me"],
+    // Replace domains with remotePatterns
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'minio.catax.me',
+        port: '',
+        pathname: '/**', // Adjust as needed for your image paths
+      },
+    ],
   },
   webpack(config, { isServer }) {
     if (isServer) {
       config.externals = config.externals || [];
-      config.externals.push("canvas");
+      config.externals.push('canvas');
     }
     return config;
   },
